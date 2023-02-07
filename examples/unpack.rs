12d91fc94470aedd57e8ec804a2f7a3eb9d51a99
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2023-02-07 21:14:40 +0000

    well done me - hotfix time!

--- examples/unpack.rs
+++ examples/unpack.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), unpak::Error> {
     let mut args = std::env::args();
     let path = args.nth(1).unwrap_or_default();
     let key = args.next();
+    let key = key.as_deref().map(str::as_bytes);
     let mut pak = unpak::Pak::load(
         || std::fs::OpenOptions::new().read(true).open(&path).ok(),
         key,
--- examples/version.rs
+++ examples/version.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), unpak::Error> {
     let mut args = std::env::args();
     let path = args.nth(1).unwrap_or_default();
     let key = args.next();
+    let key = key.as_deref().map(str::as_bytes);
     println!(
         "{}",
         unpak::Pak::load(
