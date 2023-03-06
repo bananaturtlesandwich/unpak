d859d94703967f6ba7714f50e7c9c4fe2d18e71c
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2023-03-06 20:17:52 +0000

    make path-based

--- examples/list.rs
+++ examples/list.rs
@@ -1,11 +1,12 @@
-use rayon::iter::ParallelIterator;
+use rayon::prelude::*;
 
 fn main() -> Result<(), unpak::Error> {
     let mut args = std::env::args();
     let path = args.nth(1).unwrap_or_default();
     let key = args.next();
-    unpak::Pak::new_from_path(path, key.as_deref().map(str::as_bytes))?
-        .par_entries()
+    unpak::Pak::new_any(path, key.as_deref().map(str::as_bytes))?
+        .entries()
+        .into_par_iter()
         .for_each(|entry| println!("{entry}"));
     std::io::stdin().read_line(&mut String::new())?;
     Ok(())
--- examples/unpack.rs
+++ examples/unpack.rs
@@ -1,17 +1,21 @@
 use rayon::iter::ParallelIterator;
+use rayon::prelude::*;
 use std::fs;
 
 fn main() -> Result<(), unpak::Error> {
     let mut args = std::env::args();
-    let path = args.nth(1).unwrap_or_default();
     let key = args.next();
-    let pak = unpak::Pak::new_from_path(&path, key.as_deref().map(str::as_bytes))?;
-    pak.par_entries()
+    let pak = unpak::Pak::new_any(
+        args.nth(1).unwrap_or_default(),
+        key.as_deref().map(str::as_bytes),
+    )?;
+    pak.entries()
+        .into_par_iter()
         .try_for_each(|entry| -> Result<(), unpak::Error> {
             let trimmed_path = entry.trim_start_matches('/');
             // the parent will always be a file
             fs::create_dir_all(std::path::Path::new(trimmed_path).parent().unwrap())?;
-            pak.read_from_path_to_file(&entry, &path, trimmed_path)?;
+            pak.read_to_file(&entry, trimmed_path)?;
             println!("{entry}");
             Ok(())
         })?;
