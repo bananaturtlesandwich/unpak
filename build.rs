f96443c9949e37ffbb67f1e9b30db629d42c221c
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2023-08-08 12:18:47 +0100

    fix oodle build script

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-search=oodle");
+    #[cfg(feature = "oodle")]
+    println!(
+        "cargo:rustc-link-search={}",
+        std::env::var("OODLE").unwrap_or_else(|_| panic!(r"to use the oodle feature set the OODLE variable to a folder containing the oodle static libraries (these can be obtained from any unreal engine install above 4.27 in Engine\Source\Runtime\OodleDataCompression\Sdks\2.9.8)"))
+    );
 }
